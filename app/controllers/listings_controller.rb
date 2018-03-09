bfc875e64e075f828b293ecbdcd4f056fce03aaa
chenkeong288 <chenkeong288@gmail.com>
2018-03-09 19:51:56 +0800

    added welcome controller and home page

--- app/controllers/listings_controller.rb
+++
@@ -1,5 +0,0 @@
-class ListingsController < ApplicationController
-  def index
-
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-  root "listings#index"
+  root "welcome#index"
 
   resources :appointments
   resources :providers
   resources :users
-  resources :listings
+  resources :welcome
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  
