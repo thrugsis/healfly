c669659505c9b55726139a7b5d3f25f786b05458
George Belanger <realtorgeorgeb@gmail.com>
2018-03-09 19:04:13 +0800

    facebook login working

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :appointments
   resources :providers
   resources :users
+
+  root 'welcome#index'
+
+  match 'auth/:provider/callback', to: 'sessions#create_from_omniauth', via: [:get, :post]
+match 'auth/failure', to: redirect('/'), via: [:get, :post]
+match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
