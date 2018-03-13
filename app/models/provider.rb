510dc6aecc20bb6dec050f721ca43da7554277ce
George Belanger <realtorgeorgeb@gmail.com>
2018-03-13 11:40:39 +0800

    image and pdf uploader

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,5 +1,9 @@
 class Provider < ApplicationRecord
 	has_many :appointments, :dependent => :delete_all
+
+
+  mount_uploaders :image, ImageUploader
+  mount_uploaders :qualification, QualificationUploader
   
   def provider?
     has_role?(:provider)
