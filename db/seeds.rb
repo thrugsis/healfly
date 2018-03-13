418acf4c520106c29c44e8aab90ffc25eb55302a
George Belanger <realtorgeorgeb@gmail.com>
2018-03-13 14:16:39 +0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,46 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = {}
+patient = {}
 
 
 #ActiveRecord::Base.transaction will rollback if there is an error (by raising an acception - create! or save!)
 ActiveRecord::Base.transaction do
  20.times do
-    user['username'] = Faker::Name.first_name
-    user['email'] = Faker::Internet.email
-    user['first_name'] = Faker::Name.first_name
-	user['last_name'] = Faker::Name.last_name
-  user['password'] = Faker::Color.color_name
-	user['gender'] = ["Male", "Female"].at(rand(2))
-	user['image'] = Faker::LoremPixel.image("500x500", false, 'people')
-	user['birthday'] = Faker::Date.between(100.years.ago, 18.years.ago)
-	user['phone_number'] = Faker::PhoneNumber.phone_number
+    patient['username'] = Faker::Name.first_name
+    patient['first_name'] = Faker::Name.first_name
+  patient['last_name'] = Faker::Name.last_name
+    patient['email'] = Faker::Internet.email
+	patient['gender'] = ["Male", "Female"].at(rand(2))
+	patient['phone_number'] = Faker::PhoneNumber.phone_number
+  patient['birthday'] = Faker::Date.between(100.years.ago, 18.years.ago)
+  patient['image'] = Faker::LoremPixel.image("500x500", false, 'people')
+  patient['medical_history'] = Faker::LoremPixel.image("500x500", false, 'business')
 
-   User.create(user)
+   Patient.create(patient)
  end
 end
+
+
 
 provider = {}
 
 #ActiveRecord::Base.transaction will rollback if there is an error (by raising an acception - create! or save!)
 ActiveRecord::Base.transaction do
- 20.times do
+ 20.times do 
+  provider['username'] = Faker::Name.first_name
+    provider['first_name'] = Faker::Name.first_name
+  provider['last_name'] = Faker::Name.last_name
+    provider['email'] = Faker::Internet.email
     provider['price'] = rand(10..20)
     provider['name'] = Faker::Company.name
     provider['location'] = Faker::Address.country
 	provider['treatment'] = Faker::Lorem.sentence($nbWords = 6, $variableNbWords = true)
 	provider['language'] = Faker::Address.country_code
-	provider['qualification'] = Faker::Lorem.sentence($nbWords = 6, $variableNbWords = true)
+	provider['qualification'] = Faker::LoremPixel.image("500x500", false, 'technics')
 	provider['image'] = Faker::LoremPixel.image("500x500", false, 'people') 
 
-   Provider.create(provider)
+  Provider.create(provider)
  end
 end
 
