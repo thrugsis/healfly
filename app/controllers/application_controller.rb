53473ae6645d2250699d757fd1d7216f4aa8a91c
yujunk <yujunk@gmail.com>
2018-03-15 14:21:43 +0800

    some edits on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  def patient_allowed?(action:, user:)
+    if user.patient?
+      action
+    else
+      return redirect_to , notice: "Sorry, as a Provider, you do not have access to this page."
+    end
+  end
+
   def allowed?(action:, user:)
     if user.patient? || user.provider?
       action
@@ -11,5 +19,4 @@ class ApplicationController < ActionController::Base
         return redirect_to "/", notice: "Sorry. You do not have the permission to verify a property."
     end
   end
-
 end
