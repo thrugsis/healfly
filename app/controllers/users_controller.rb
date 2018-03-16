026b9080dc9578b116dde6e2c3fc3aee3d32ba49
yujunk <yujunk@gmail.com>
2018-03-16 16:09:27 +0800

    added alerts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,19 +31,22 @@ class UsersController < Clearance::UsersController
       @user = Provider.new(user_params)
     end
    
-    if @user.save
-      sign_in(@user)
-      if @user.patient?
-        redirect_to edit_patient_path(@user)
+    respond_to do |format|
+      if @user.save
+        sign_in(@user)
+        if @user.patient?
+          redirect_to edit_patient_path(@user)
+        else
+          redirect_to edit_provider_path(@user) 
+        end
       else
-        redirect_to edit_provider_path(@user)
-      end
-    else
-      flash.now[:error] = "Email already taken"
-      @user = User.new(user_params)
-      render action: "new"
-    end 
+        format.js 
+        # @user = User.new(user_params) #where to put this
+      end 
+    end
   end
+
+  # format.html { redirect_to root_url, notice: 'Email already taken.' }
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
