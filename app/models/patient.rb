bf11c709f048c4cfcfa30d0cd575466425c6be66
George Belanger <realtorgeorgeb@gmail.com>
2018-03-19 11:28:24 +0800

    fixed profile picture

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,8 +2,10 @@ class Patient < User
 
   has_many :appointments, :dependent => :delete_all
 
-  mount_uploaders :image, ImageUploader
   mount_uploaders :medical_history, MedicalHistoryUploader
-  mount_uploaders :default_picture, DefaultPictureUploader
+    mount_uploaders :image, ImageUploader
+      mount_uploader :profile_picture, ProfilePictureUploader
+
+
 
 end
